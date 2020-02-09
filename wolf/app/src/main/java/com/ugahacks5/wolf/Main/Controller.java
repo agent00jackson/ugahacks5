935bee6ef667b925508a175ee537723b2abf7bfa
Drew Taylor <raylotdreygone@gmail.com>
2020-02-09 01:26:28 -0500

    Created get methods in the controller class

--- wolf/app/src/main/java/com/ugahacks5/wolf/Main/Controller.java
+++ wolf/app/src/main/java/com/ugahacks5/wolf/Main/Controller.java
@@ -16,20 +16,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class Controller {
 
     static final String BASE_URL = "https://hackathon-1569554271539.appspot.com/";
+    HackathonAPI api;
 
-    public void start() {
-        Gson gson = new GsonBuilder()
-                .setLenient()
-                .create();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        HackathonAPI hackathonAPI = retrofit.create(HackathonAPI.class);
-
-        Call<List<StockInfo>> call = hackathonAPI.loadStack("status:open");
+    public void getStackInfo(String stackItem){
+        Call<List<StockInfo>> call = api.loadStackInfo(stackItem);
         call.enqueue(new Callback<List<StockInfo>>() {
             @Override
             public void onResponse(Call<List<StockInfo>> call, Response<List<StockInfo>> response) {
@@ -49,6 +40,68 @@ public class Controller {
                 t.printStackTrace();
             }
         });
+    }
+
+    public void getPortfolio(){
+        Call<List<StockInfo>> call = api.loadStack("status:open");
+        call.enqueue(new Callback<List<StockInfo>>() {
+            @Override
+            public void onResponse(Call<List<StockInfo>> call, Response<List<StockInfo>> response) {
+                if(response.isSuccessful()) {
+                    List<StockInfo> changesList = response.body();
+                    for(StockInfo s : changesList)
+                    {
+                        Log.d("d",s.getTicker());
+                    }
+                } else {
+                    Log.d("d",response.errorBody().toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<StockInfo>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public void getPosition(){
+
+        Call<List<Position>> call = api.loadPortfolio("status:open");
+        call.enqueue(new Callback<List<Position>>() {
+            @Override
+            public void onResponse(Call<List<Position>> call, Response<List<Position>> response) {
+                if (response.isSuccessful()) {
+                    List<Position> changesList = response.body();
+                    for (Position p : changesList) {
+                        Log.d("p", p.getTicker());
+                    }
+                } else {
+                    Log.d("p", response.errorBody().toString());
+                }
+            }
+
+
+            @Override
+            public void onFailure(Call<List<Position>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+    }
+
+    public void start() {
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        api = retrofit.create(HackathonAPI.class);
+
+
 
 
     }
--- wolf/app/src/main/java/com/ugahacks5/wolf/Main/HackathonAPI.java
+++ wolf/app/src/main/java/com/ugahacks5/wolf/Main/HackathonAPI.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface HackathonAPI {
@@ -12,6 +13,9 @@ public interface HackathonAPI {
 
     @GET("portfolio")
     Call<List<Position>> loadPortfolio(@Query("q") String status);
+
+    @GET("info?ticker={StockName}")
+    Call<List<StockInfo>> loadStackInfo(@Path("StockName") String stockName);
 
 
 }

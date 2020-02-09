efd95e3df0ddbaef92b626ee02ace85fe8ad3d88
Jackson Randolph <agent00jackson@gmail.com>
2020-02-08 21:26:09 -0500

    Stuff

--- web/app.py
+++ web/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, json
+from flask import Flask, json, request
 
 companyStack = [
-    ("AMZN", "Amazon"),
-    ("MSFT", "Microsoft"),
-    ("TSLA", "Tesla"),
-    ("SYNA", "Synaptics"),
-    ("PINS", "Pinterest"),
-    ("LYFT", "Lyft"),
+    ("AMZN", "Amazon", "url", "free two day shipping"),
+    ("MSFT", "Microsoft", "url", "billy g."),
+    ("TSLA", "Tesla", "url", "praise be unto musk"),
+    ("SYNA", "Synaptics", "url", "mouse"),
+    ("PINS", "Pinterest", "url", "we gon craft"),
+    ("LYFT", "Lyft", "url", "yeeee haaaw")
 ]
 portfolio = [
     ("GOOG", 50),
@@ -16,9 +16,30 @@ portfolio = [
 
 api = Flask(__name__)
 
-@api.route('/companies', methods=['GET'])
+@api.route('/stack', methods=['GET'])
 def get_companies():
-  return json.dumps(companyStack)
+    return json.dumps(companyStack)
+
+@api.route('/portfolio', methods=['GET'])
+def get_portfolio():
+    return json.dumps(portfolio)
+
+@api.route('/swipe', methods=['POST'])
+def post_swipe():
+    #
+    return json.dumps({"success": True}), 201
+
+def loadStack():
+    #get list of tickers
+
+    #for each ticker:
+        #grab company name
+        #grab wikipedia description
+        #grab picture url
+        #create tuple (ticker, name, desc, picture url)
+        #add tuple to companyStack
+    return
 
 if __name__ == '__main__':
-    api.run()
+    loadStack()
+    api.run(host='0.0.0.0', port=8080)
---
+++ web/bannerImageAPI.py
@@ -0,0 +1,11 @@
+import beautifulsoup4
+
+def get_banner():
+film_id = '0423409'
+url = 'http://www.imdb.com/title/tt%s/' % (film_id)
+soup = BeautifulSoup(urllib2.urlopen(url).read())
+link = soup.find(itemprop="image")
+print(link["src"])
+
+if __name__ == '__main__':
+    api.run()
